656286145ea0771e5c2dc66d1c6da087f35c9d3d
Dmitry Semenov <lonelyteapot@gmail.com>
2023-09-06 18:01:06 +0300

    Transparentize the AppBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:open_transit_app/map_widget.dart';
 
 void main() {
@@ -17,16 +18,28 @@ class MainApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0xFF25A18E)),
         useMaterial3: true,
       ),
-      home: Builder(
-        builder: (context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: const Text('Open Transit'),
-              backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-            ),
-            body: const CustomMapWidget(),
-          );
-        },
+      home: Scaffold(
+        extendBodyBehindAppBar: true,
+        appBar: AppBar(
+          title: const Text('Open Transit'),
+          forceMaterialTransparency: true,
+          centerTitle: true,
+          systemOverlayStyle: const SystemUiOverlayStyle(
+            statusBarColor: Colors.transparent,
+            statusBarIconBrightness: Brightness.dark,
+          ),
+        ),
+        drawerEnableOpenDragGesture: false,
+        drawer: Drawer(
+          child: ListView(
+            children: const [
+              ListTile(
+                title: Text('Open Transit'),
+              ),
+            ],
+          ),
+        ),
+        body: const CustomMapWidget(),
       ),
     );
   }

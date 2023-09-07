5cf5a4c3717ccf6da5da39fd78d44ad0286e1142
Dmitry Semenov <lonelyteapot@gmail.com>
2023-09-07 22:16:30 +0300

    Fix CORS issue that affected Firefox
    Try custom http handler
    
    Try custom http handler
    
    Properly change dependencies

--- lib/map_widget.dart
+++ lib/map_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
+import 'package:flutter_map_cancellable_tile_provider/flutter_map_cancellable_tile_provider.dart';
 import 'package:latlong2/latlong.dart';
 
 class CustomMapWidget extends StatefulWidget {
@@ -49,6 +50,7 @@ class _CustomMapWidgetState extends State<CustomMapWidget> {
       ],
       children: [
         TileLayer(
+          tileProvider: CancellableNetworkTileProvider(),
           urlTemplate: mapboxUrlTemplate,
           userAgentPackageName: 'open_transit.open_transit_app',
           maxZoom: 18,

ebe0c80bbbf9d8e88c8e17c15b4252893fd52771
Dmitry Semenov <lonelyteapot@gmail.com>
2023-09-23 05:21:16 +0300

    Change theme selector

--- lib/src/home/drawer_widget.dart
+++ lib/src/home/drawer_widget.dart
@@ -1,65 +1,41 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
 import 'package:url_launcher/link.dart';
 
 import '../settings/settings_provider.dart';
 import '../web_utils/web_utils.dart';
 
-final _apkDownloadUrl =
-    WebUtils.instance?.getBaseUrl()?.resolve('open-transit-app.apk');
+part 'drawer_widget.g.dart';
+
+@riverpod
+Uri? apkDownloadUrl(ApkDownloadUrlRef ref) {
+  // Non-null only on Web
+  return WebUtils.instance?.getBaseUrl()?.resolve('open-transit-app.apk');
+}
 
 class DrawerContent extends ConsumerWidget {
   const DrawerContent({super.key});
 
-  bool _shouldShowAndroidAppDownloadLink() {
-    return _apkDownloadUrl != null &&
-        defaultTargetPlatform != TargetPlatform.iOS;
-  }
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final apkDownloadUrl = ref.watch(apkDownloadUrlProvider);
     return Column(
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        if (_shouldShowAndroidAppDownloadLink())
-          _AndroidAppDownloadLink(url: _apkDownloadUrl),
+        if (apkDownloadUrl != null)
+          _AndroidAppDownloadLink(url: apkDownloadUrl),
         const Spacer(),
         SwitchListTile(
-          title: const Text('Debug info'),
+          title: const Text('Show debugging info'),
           value: ref.watch(settingsProvider).showDebugInfo,
           onChanged: (value) {
             ref.read(settingsProvider.notifier).setShowDebugInfo(value);
           },
         ),
-        const Padding(
-          padding: EdgeInsets.symmetric(vertical: 4),
-          child: Center(child: Text('Theme')),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(16).copyWith(top: 0),
-          child: SegmentedButton<ThemeMode>(
-            showSelectedIcon: false,
-            selected: {ref.watch(settingsProvider).themeMode},
-            onSelectionChanged: (value) {
-              ref.read(settingsProvider.notifier).setThemeMode(value.single);
-            },
-            segments: const [
-              ButtonSegment(
-                value: ThemeMode.light,
-                label: Text('Light'),
-              ),
-              ButtonSegment(
-                value: ThemeMode.system,
-                label: Text('Auto'),
-              ),
-              ButtonSegment(
-                value: ThemeMode.dark,
-                label: Text('Dark'),
-              ),
-            ],
-          ),
-        ),
+        const SizedBox(height: 8),
+        const _ThemeSelector(),
       ],
     );
   }
@@ -73,7 +49,6 @@ class _AndroidAppDownloadLink extends StatelessWidget {
   final Uri? url;
 
   Widget? _buildSubtitle() {
-    assert(kIsWeb, 'Expected to be on the web platform');
     if (defaultTargetPlatform == TargetPlatform.android) {
       return const Text('for smoother experience');
     }
@@ -93,6 +68,60 @@ class _AndroidAppDownloadLink extends StatelessWidget {
           onTap: followLink,
         );
       },
+    );
+  }
+}
+
+class _ThemeSelector extends ConsumerWidget {
+  const _ThemeSelector({
+    // ignore: unused_element
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeMode = ref.watch(settingsProvider).themeMode;
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16).copyWith(bottom: 16),
+      child: SegmentedButton<ThemeMode>(
+        showSelectedIcon: false,
+        selected: {themeMode},
+        onSelectionChanged: (value) {
+          ref.read(settingsProvider.notifier).setThemeMode(value.single);
+        },
+        style: const ButtonStyle(
+          iconSize: MaterialStatePropertyAll<double>(24),
+        ),
+        segments: [
+          ButtonSegment(
+            value: ThemeMode.light,
+            icon: Icon(
+              themeMode == ThemeMode.light
+                  ? Icons.light_mode
+                  : Icons.light_mode_outlined,
+            ),
+            tooltip: 'Switch to light theme',
+          ),
+          ButtonSegment(
+            value: ThemeMode.system,
+            icon: Icon(
+              themeMode == ThemeMode.system
+                  ? Icons.brightness_auto
+                  : Icons.brightness_auto_outlined,
+            ),
+            tooltip: 'Switch to system-defined theme',
+          ),
+          ButtonSegment(
+            value: ThemeMode.dark,
+            icon: Icon(
+              themeMode == ThemeMode.dark
+                  ? Icons.dark_mode
+                  : Icons.dark_mode_outlined,
+            ),
+            tooltip: 'Switch to dark theme',
+          ),
+        ],
+      ),
     );
   }
 }
---
+++ lib/src/home/drawer_widget.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'drawer_widget.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$apkDownloadUrlHash() => r'36942c31212424d37de0cf45c5afff26d8c5bad4';
+
+/// See also [apkDownloadUrl].
+@ProviderFor(apkDownloadUrl)
+final apkDownloadUrlProvider = AutoDisposeProvider<Uri?>.internal(
+  apkDownloadUrl,
+  name: r'apkDownloadUrlProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$apkDownloadUrlHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef ApkDownloadUrlRef = AutoDisposeProviderRef<Uri?>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member

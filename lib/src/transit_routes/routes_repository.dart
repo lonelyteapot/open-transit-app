8956351e40a349043982503fc3f4bb27be10a48b
Dmitry Semenov <lonelyteapot@gmail.com>
2023-10-09 16:24:14 +0300

    Update Routes view

--- lib/src/transit_routes/routes_repository.dart
+++ lib/src/transit_routes/routes_repository.dart
@@ -1,10 +1,12 @@
 class TransitRoute {
   const TransitRoute({
     required this.id,
+    required this.number,
     required this.name,
   });
 
   final String id;
+  final String number;
   final String name;
 }
 
@@ -13,24 +15,29 @@ class TransitRoutesRepository {
     await Future.delayed(const Duration(seconds: 2));
     return [
       const TransitRoute(
-        id: 'route1',
-        name: 'Route 1',
+        id: '8d5d61f9-66e4-49f0-aa42-dce7217499f7',
+        number: 'A-1',
+        name: 'Harmony Sq - Elmwood Ln',
       ),
       const TransitRoute(
-        id: 'route2',
-        name: 'Route 2',
+        id: 'f9d2439a-7d27-471f-94d0-ec87be5e86f5',
+        number: 'A-2',
+        name: 'Rosebud Ln - Oak Ave',
       ),
       const TransitRoute(
-        id: 'route3',
-        name: 'Route 3',
+        id: '5e3d6b5a-15b2-4d4b-a509-80b6a1ecf8f8',
+        number: 'T-3',
+        name: 'Sunflower Sq - Maple Ave',
       ),
       const TransitRoute(
-        id: 'route4',
-        name: 'Route 4',
+        id: '7e62d2a7-0b35-4e96-9f52-ef6b7e0a3e4d',
+        number: '4',
+        name: 'Oak Ave - Magnolia St',
       ),
       const TransitRoute(
-        id: 'route5',
-        name: 'Route 5',
+        id: '4f9e3d84-1082-4a7f-91ef-573c8f3a07c7',
+        number: 'Tl-5',
+        name: 'Maple Ave - Serenity Sq',
       ),
     ];
   }
--- lib/src/transit_routes/routes_view.dart
+++ lib/src/transit_routes/routes_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../core/providers.dart';
 import 'routes_provider.dart';
+import 'routes_repository.dart';
 
 class RoutesView extends ConsumerWidget {
   const RoutesView({super.key});
@@ -10,7 +11,8 @@ class RoutesView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final transitRoutes = ref.watch(transitRoutesProvider);
-    return DecoratedBox(
+    return Container(
+      padding: const EdgeInsets.only(top: 12),
       decoration: BoxDecoration(
         color: Theme.of(context).colorScheme.background,
         borderRadius: const BorderRadius.vertical(top: Radius.circular(28)),
@@ -22,17 +24,49 @@ class RoutesView extends ConsumerWidget {
           AsyncData(value: final routes) => ListView.builder(
               itemCount: routes.length,
               itemBuilder: (context, index) {
-                final route = routes[index];
-                return ListTile(
-                  leading: const Icon(Icons.route),
-                  title: Text(route.name),
-                );
+                return _RouteTile(route: routes[index]);
               },
             ),
           AsyncError(:final error) => Center(child: Text('Error: $error')),
           _ => const Center(child: CircularProgressIndicator()),
         },
       ),
+    );
+  }
+}
+
+class _RouteTile extends StatelessWidget {
+  const _RouteTile({
+    required this.route,
+  });
+
+  final TransitRoute route;
+
+  void handlePress() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      onTap: handlePress,
+      leading: FilledButton.tonal(
+        onPressed: handlePress,
+        child: ConstrainedBox(
+          // TODO: Dynamic synced width
+          constraints: const BoxConstraints(minWidth: 56),
+          child: Text(
+            route.number,
+            style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                  fontFamily: 'monospace',
+                ),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+      title: Text(
+        route.name,
+        style: Theme.of(context).textTheme.bodyMedium,
+      ),
+      trailing: const Icon(Icons.directions_bus),
     );
   }
 }

e4456230b04c49dd6489d114f92e1c2caee32421
Dmitry Semenov <lonelyteapot@gmail.com>
2023-10-29 02:34:32 +0300

    Upgrade dependencies

--- lib/src/core/graphql.g.dart
+++ lib/src/core/graphql.g.dart
@@ -6,7 +6,7 @@ part of 'graphql.dart';
 // RiverpodGenerator
 // **************************************************************************
 
-String _$graphqlClientHash() => r'b2bd5307d04de9be42c932ed5e064e764aba13a8';
+String _$graphqlClientHash() => r'0f0c2323e68b6e34530cb9815e88f4a4fc558957';
 
 /// See also [graphqlClient].
 @ProviderFor(graphqlClient)
@@ -22,4 +22,4 @@ final graphqlClientProvider = Provider<GraphQLClient>.internal(
 
 typedef GraphqlClientRef = ProviderRef<GraphQLClient>;
 // ignore_for_file: type=lint
-// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, inference_failure_on_uninitialized_variable, inference_failure_on_function_return_type, inference_failure_on_untyped_parameter, deprecated_member_use_from_same_package
